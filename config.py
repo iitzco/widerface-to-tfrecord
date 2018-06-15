030b528188fbda67203d691ab748622b3c030b6e
Ivan Itzcovich <i.itzcovich@gmail.com>
2018-06-15 12:22:17 -0300

    Add improvements and support for train/val/test

---
+++ config.py
@@ -0,0 +1,14 @@
+# Training
+TRAIN_WIDER_PATH = "./WIDER_train/"
+
+# Validation
+VAL_WIDER_PATH = "./WIDER_val/"
+
+# Testing
+TEST_WIDER_PATH = "./WIDER_test/"
+
+# Ground Truth
+GROUND_TRUTH_PATH = "./wider_face_split/"
+
+# Output
+OUTPUT_PATH = "./output/"
--- wider_to_tfrecord.py
+++ wider_to_tfrecord.py
@@ -8,113 +8,134 @@ import cv2
 import os
 import hashlib
 
+import config
 from utils import dataset_util
 
 
-flags = tf.app.flags
-flags.DEFINE_string('output_path', 'output/val.tfrecord', 'Path to output TFRecord')
-FLAGS = flags.FLAGS
+def parse_example(f, images_path):
+    height = None # Image height
+    width = None # Image width
+    filename = None # Filename of the image. Empty if image is not from file
+    encoded_image_data = None # Encoded image bytes
+    image_format = b'jpeg' # b'jpeg' or b'png'
 
-def parse_example(f):
-  height = None # Image height
-  width = None # Image width
-  filename = None # Filename of the image. Empty if image is not from file
-  encoded_image_data = None # Encoded image bytes
-  image_format = b'jpeg' # b'jpeg' or b'png'
+    xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = [] # List of normalized right x coordinates in bounding box (1 per box)
+    ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = [] # List of normalized bottom y coordinates in bounding box (1 per box)
+    classes_text = [] # List of string class name of bounding box (1 per box)
+    classes = [] # List of integer class id of bounding box (1 per box)
+    poses = []
+    truncated = []
+    difficult_obj = []
 
-  xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
-  xmaxs = [] # List of normalized right x coordinates in bounding box (1 per box)
-  ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
-  ymaxs = [] # List of normalized bottom y coordinates in bounding box (1 per box)
-  classes_text = [] # List of string class name of bounding box (1 per box)
-  classes = [] # List of integer class id of bounding box (1 per box)
-  poses = []
-  truncated = []
-  difficult_obj = []
+    filename = f.readline().rstrip()
+    if not filename:
+        raise IOError()
 
+    filepath = os.path.join(images_path, filename)
 
+    image_raw = cv2.imread(filepath)
 
-  filename = f.readline().rstrip()
-  print(filename)
-  # filepath_b = os.path.join(b"../face/WIDER_train/images/", filename.encode('utf-8'))
-  filepath = os.path.join("../face/WIDER_val/images/", filename)
-  print(filepath)
-  image_raw = cv2.imread(filepath)
+    encoded_image_data = open(filepath, "rb").read()
+    key = hashlib.sha256(encoded_image_data).hexdigest()
 
-  encoded_image_data = open(filepath, "rb").read()
-  key = hashlib.sha256(encoded_image_data).hexdigest()
+    height, width, channel = image_raw.shape
 
-  height, width, channel = image_raw.shape
-  print("height is %d, width is %d, channel is %d" % (height, width, channel))
+    face_num = int(f.readline().rstrip())
+    if not face_num:
+        raise Exception()
+    valid_face_num = 0
 
-  face_num = int(f.readline().rstrip())
-  valid_face_num = 0
+    for i in range(face_num):
+        annot = f.readline().rstrip().split()
+        if not annot:
+            raise Exception()
 
-  for i in range(face_num):
-    annot = f.readline().rstrip().split()
-    # WIDER FACE DATASET CONTAINS SOME ANNOTATIONS WHAT EXCEEDS THE IMAGE BOUNDARY
-    if(float(annot[2]) > 25.0):
-        if(float(annot[3]) > 30.0):
-            xmins.append( max(0.005, (float(annot[0]) / width) ) )
-            ymins.append( max(0.005, (float(annot[1]) / height) ) )
-            xmaxs.append( min(0.995, ((float(annot[0]) + float(annot[2])) / width) ) )
-            ymaxs.append( min(0.995, ((float(annot[1]) + float(annot[3])) / height) ) )
-            classes_text.append(b'face')
-            classes.append(1)
-            poses.append("front".encode('utf8'))
-            truncated.append(int(0))
-            print(xmins[-1], ymins[-1], xmaxs[-1], ymaxs[-1], classes_text[-1], classes[-1])
-            valid_face_num += 1;
-
-  print("Face Number is %d" % face_num)
-  print("Valid face number is %d" % valid_face_num)
+        # WIDER FACE DATASET CONTAINS SOME ANNOTATIONS WHAT EXCEEDS THE IMAGE BOUNDARY
+        if(float(annot[2]) > 25.0):
+                if(float(annot[3]) > 30.0):
+                        xmins.append( max(0.005, (float(annot[0]) / width) ) )
+                        ymins.append( max(0.005, (float(annot[1]) / height) ) )
+                        xmaxs.append( min(0.995, ((float(annot[0]) + float(annot[2])) / width) ) )
+                        ymaxs.append( min(0.995, ((float(annot[1]) + float(annot[3])) / height) ) )
+                        classes_text.append(b'face')
+                        classes.append(1)
+                        poses.append("front".encode('utf8'))
+                        truncated.append(int(0))
+                        valid_face_num += 1;
 
 
-  tf_example = tf.train.Example(features=tf.train.Features(feature={
-    'image/height': dataset_util.int64_feature(int(height)),
-    'image/width': dataset_util.int64_feature(int(width)),
-    'image/filename': dataset_util.bytes_feature(filename.encode('utf-8')),
-    'image/source_id': dataset_util.bytes_feature(filename.encode('utf-8')),
-    'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
-    'image/encoded': dataset_util.bytes_feature(encoded_image_data),
-    'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
-    'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-    'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-    'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-    'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-    'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-    'image/object/class/label': dataset_util.int64_list_feature(classes),
-    'image/object/difficult': dataset_util.int64_list_feature(int(0)),
-    'image/object/truncated': dataset_util.int64_list_feature(truncated),
-    'image/object/view': dataset_util.bytes_list_feature(poses),
-    }))
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(int(height)),
+        'image/width': dataset_util.int64_feature(int(width)),
+        'image/filename': dataset_util.bytes_feature(filename.encode('utf-8')),
+        'image/source_id': dataset_util.bytes_feature(filename.encode('utf-8')),
+        'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+        'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+        'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+        'image/object/difficult': dataset_util.int64_list_feature(int(0)),
+        'image/object/truncated': dataset_util.int64_list_feature(truncated),
+        'image/object/view': dataset_util.bytes_list_feature(poses),
+        }))
 
 
-  return valid_face_num, tf_example
+    return valid_face_num, tf_example
+
+
+def run(images_path, ground_truth_file, output_path):
+    f = open(ground_truth_file)
+    writer = tf.python_io.TFRecordWriter(output_path)
+
+    valid_image_num = 0
+    invalid_image_num = 0
+
+    print("Processing {}".format(images_path))
+    while True:
+        try:
+            valid_face_number, tf_example = parse_example(f, images_path)
+            if(valid_face_number > 0):
+                writer.write(tf_example.SerializeToString())
+                valid_image_num += 1
+            else:
+                invalid_image_num += 1
+        except IOError:
+            break
+        except Exception:
+            raise
+
+    writer.close()
+
+    print("Correctly created record for {} images.".format(valid_image_num)
 
 
 def main(unused_argv):
-  f = open("../face/wider_face_split/wider_face_val_bbx_gt.txt")
-  writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    # Training
+    if config.TRAIN_WIDER_PATH is not None:
+        images_path = os.path.join(config.TRAIN_WIDER_PATH, "images")
+        ground_truth_file = os.path.join(config.GROUND_TRUTH_PATH, "wider_face_train_bbx_gt.txt")
+        output_path = os.path.join(config.OUTPUT_PATH, "train.tfrecord")
+        run(images_path, ground_truth_file, output_path)
 
-# WIDER FACE DATASET ANNOTATED 12880 IMAGES
-  valid_image_num = 0
-  invalid_image_num = 0
-  for image_idx in range(12880):
-    print("image idx is %d" % image_idx)
-    valid_face_number, tf_example = parse_example(f)
-    if(valid_face_number != 0):
-      writer.write(tf_example.SerializeToString())
-      valid_image_num += 1
-    else:
-      invalid_image_num += 1
-      print("Pass!")
-  writer.close()
+    # Validation
+    if config.VAL_WIDER_PATH is not None:
+        images_path = os.path.join(config.VAL_WIDER_PATH, "images")
+        ground_truth_file = os.path.join(config.GROUND_TRUTH_PATH, "wider_face_val_bbx_gt.txt")
+        output_path = os.path.join(config.OUTPUT_PATH, "val.tfrecord")
+        run(images_path, ground_truth_file, output_path)
 
-  print("Valid image number is %d" % valid_image_num)
-  print("Invalid image number is %d" % invalid_image_num)
-
-
+    # Testing
+    if config.TRAIN_WIDER_PATH is not None:
+        images_path = os.path.join(config.TEST_WIDER_PATH, "images")
+        ground_truth_file = os.path.join(config.GROUND_TRUTH_PATH, "wider_face_test_bbx_gt.txt")
+        output_path = os.path.join(config.OUTPUT_PATH, "test.tfrecord")
+        run(images_path, ground_truth_file, output_path)
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
